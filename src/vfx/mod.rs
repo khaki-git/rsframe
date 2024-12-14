5e51bf47d38d8d87b6fe1d8af5b7252bae811229
WeirdOddz <OscarPeachMan+github@outlook.com>
2024-12-14 17:35:39 +1100

    remove dummy video

--- src/vfx/mod.rs
+++ src/vfx/mod.rs
@@ -2,8 +2,6 @@
 mod tmp;
 /// Core library for converting frames into a video.
 mod compile;
-/// Formerly used module, originally meant for generating placeholder videos but now obsolete.
-mod dummyvideo;
 /// Text managing module.
 mod text;
 /// Main interface for editing videos using `rsframe`
